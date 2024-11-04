0f8a82c72fc568073026a613b8a52e8929c61913
Nidhi <nidhikatkani3@gmail.com>
2024-11-04 17:12:51 +0530

    footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,12 @@
+<style>
+    .aai-footer-nav-title{
+        color: white;
+    }
+</style>
+
+
 <!-- Footer Start -->
+
 <footer
     class="aai-footer pt-120"
     style=" background: url('assets/img/bg/footer-bg.jpeg') no-repeat center
@@ -6,27 +14,30 @@
     <div class="container">
 
         <div class="aai-footer-navigation-widgets py-80">
-            <div class="row">
+            <div class="row pt-5">
 
                 <div
-                    class="col-xl-4 col-lg-4 col-md-4 mb-5 mb-lg-0"
+                    class="col-xl-3 col-lg-4 col-md-3 mb-5 mb-lg-0"
                     data-aos="fade-up"
                     data-aos-delay="100"
                     data-aos-duration="1500">
                     <nav class="aai-footer-nav">
-                        <h3 class="aai-footer-nav-title">Sitemap</h3>
-                        <ul class="aai-footer-nav-list">
+                        <h3 class="aai-footer-nav-title">Company</h3>
+                        <ul class="aai-footer-nav-list" style="list-style-type: none;" >
                             <li class="aai-footer-nav-list-item">
-                                <a href="main" class="aai-footer-nav-link"> Home </a>
+                                <a href="" class="aai-footer-nav-link">Our Team </a>
                             </li>
                             <li class="aai-footer-nav-list-item">
-                                <a href="about" class="aai-footer-nav-link"> About Us </a>
+                                <a href="" class="aai-footer-nav-link">Why Infinity Globus Australia </a>
                             </li>
                             <li class="aai-footer-nav-list-item">
-                                <a href="service1" class="aai-footer-nav-link">Services</a>
+                                <a href="service1" class="aai-footer-nav-link">Data Security & Confidentiality</a>
                             </li>
                             <li class="aai-footer-nav-list-item">
-                                <a href="product1" class="aai-footer-nav-link"> Product</a>
+                                <a href="product1" class="aai-footer-nav-link"> Careers</a>
+                            </li>
+                            <li class="aai-footer-nav-list-item">
+                                <a href="contact" class="aai-footer-nav-link">Contact</a>
                             </li>
                             <!-- <li class="aai-footer-nav-list-item">
                         <a href="features.html" class="aai-footer-nav-link"> Feature </a>
@@ -35,94 +46,80 @@
                     </nav>
                 </div>
                 <div
-                    class="col-xl-4 col-lg-4 col-md-4 mb-5 mb-lg-0"
+                    class="col-xl-3 col-lg-4 col-md-3 mb-5 mb-lg-0"
                     data-aos="fade-up"
                     data-aos-delay="300"
                     data-aos-duration="1500">
 
 
                     <nav class="aai-footer-nav">
-                        <h3 class="aai-footer-nav-title">Useful Links</h3>
-                        <ul class="aai-footer-nav-list">
+                        <h3 class="aai-footer-nav-title">Quick Links</h3>
+                        <ul class="aai-footer-nav-list"  style="list-style-type: none;" >
 
                             <li class="aai-footer-nav-list-item">
-                                <a href="privacy" class="aai-footer-nav-link">
-                                    Privacy Policy
+                                <a href="service" class="aai-footer-nav-link">
+                                Services
                                 </a>
                             </li>
                             <li class="aai-footer-nav-list-item">
-                                <a href="terms" class="aai-footer-nav-link">Terms & Services </a>
+                                <a href="faq" class="aai-footer-nav-link">FAQs</a>
                             </li>
-                            <li class="aai-footer-nav-list-item">
-                                <a href="contact" class="aai-footer-nav-link">Contact</a>
-                            </li>
+                           
                         </ul>
-                        <div class="aai-social-links d-flex pt-md-3 pt-2">
-                            <a href="http://" target="_blank" rel="noopener noreferrer">
-                                <i class="fa-brands fa-facebook-f"></i>
-                            </a>
-                            <a href="http://" target="_blank" rel="noopener noreferrer">
-                                <i class="fa-brands fa-twitter"></i></a>
-                            <a href="http://" target="_blank" rel="noopener noreferrer">
-                                <i class="fa-brands fa-instagram"></i></a>
-                            <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-youtube"></i></a>
-                        </div>
+                        
                     </nav>
                 </div>
- 
                 <div
-             class="col-xl-4 col-lg-4 col-md-12"
-             data-aos="fade-right"
-             data-aos-duration="1500"
-             data-aos-delay="50">
-             <div class="aai-footer-info">
-               <a href="" class="aai-footer-logo">
-                 <img src="assets/image/logo.png" alt="" />
-               </a>
-               <div class="info mt-4 col-md-12  row" style="color:white;">
-                 <div class="phone row col-md-12 col-12 m-0 pt-md-3 pt-2" style="align-items: center;">
-                   <h6 class="col-md-2 col-2" style="font-weight: 700;"><span><i class="bi bi-phone"></i>
-                       &nbsp;</span>
-                   </h6>
-                   <p class="col-md-10 col-10 p-0">
-                     <a href="" style="color:white !important;">+91 98765 43210</a><br>
-                   </p>
-                 </div>
-                 <div class="email row col-md-12 m-0 pt-md-3 pt-2" style="align-items: center;">
-                   <h6 class="col-md-2 col-2" style="font-weight: 700;"><span><i
-                         class="bi bi-globe-americas"></i>
-                       &nbsp;</span></h6>
-                   <p class="col-md-10 col-10 p-0"> admin@prosoft.org.in
-                   </p>
-                 </div>
-                 <div class="address row col-md-12 m-0 pt-md-3 pt-2" style="align-items: center;">
-                   <h6 class="col-md-2 col-2" style="font-weight: 700;"><span><i class="bi bi-geo-alt"></i>
-                     </span></h6>
-                   <p class="col-md-10 col-10 p-0">PROSOFT LLC,
-                     Office No 10, Level 1,
-                     Sharjah Media City, Sharjah,UAE</p>
-                 </div>
-               </div>
-               
-             </div>
-           </div>
-           <div class="col-xl-8 col-lg-8 col-md-12 mt-5 mt-lg-0">
-             <div class="row">
-               <div
-                 class="col-xl-4 col-lg-4 col-md-4"
-                 data-aos="fade-up"
-                 data-aos-delay="500"
-                 data-aos-duration="1500">
+                    class="col-xl-3 col-lg-4 col-md-3 mb-5 mb-lg-0"
+                    data-aos="fade-up"
+                    data-aos-delay="300"
+                    data-aos-duration="1500">
 
-               </div>
 
+                    <nav class="aai-footer-nav">
+                        <h3 class="aai-footer-nav-title">RESOURCES</h3>
+                        <ul class="aai-footer-nav-list"  style="list-style-type: none;" >
+
+                            <li class="aai-footer-nav-list-item">
+                                <a href="blog" class="aai-footer-nav-link">
+                                Blog
+                                </a>
+                            </li>
+                           
+                        </ul>
+                        
+                    </nav>
+                </div>
+
+                <div
+                    class="col-xl-3 col-lg-4 col-md-3 mb-5 mb-lg-0"
+                    data-aos="fade-up"
+                    data-aos-delay="300"
+                    data-aos-duration="1500">
+                    <div class="info mt-4 col-md-12  row" style="color:white;">
+                            <div class="phone row col-md-12 col-12 m-0 pt-md-1 pt-2" style="align-items: center;">
+                             <p class="m-0">CALL</p>
+                                 <p class="m-0">
+                                    <a href="" style="color:white !important;">+61 370443376</a><br>
+                                </p>
+                            </div>
+                            <div class="email row col-md-12 m-0 pt-md-1 pt-2" style="align-items: center;">
+                             <p class="m-0">EMAIL</p>
+                                 <p class="m-0">au@infinity-globus.com </p>
+                            </div>
+                            <div class="address row col-md-12 m-0 pt-md-1 pt-2" style="align-items: center;">
+                              <p class="m-0">GET IN TOUCH</p>
+                                 <p class="m-0">  Suite 28/195 Wellington Road, Clayton, VIC 3168 Australia</p>
+                            </div>
+                        </div>
+
+                </div>
+         
             </div>
         </div>
-    </div>
-    </div>
-    <div class="aai-footer-copyright text-center">
-        <p class="aai-copyright-text">Copyright @2023 Prosoft</p>
-    </div>
+        <div class="aai-footer-copyright text-center">
+            <p class="aai-copyright-text">Copyright @2023 GLOBUS</p>
+        </div>
     </div>
 </footer>
 
